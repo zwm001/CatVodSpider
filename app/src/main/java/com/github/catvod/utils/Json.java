e7a0c5103ba34dc7e9801213fba0f20650a61cbf
zwm001 <110072013+zwm001@users.noreply.github.com>
2025-03-20 15:36:51 +0800

    Update Json.java

--- app/src/main/java/com/github/catvod/utils/Json.java
+++ app/src/main/java/com/github/catvod/utils/Json.java
@@ -1,8 +1,13 @@
 package com.github.catvod.utils;
 
+import com.github.catvod.crawler.SpiderDebug;
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import java.lang.reflect.Type;
 
 public class Json {
 
@@ -22,4 +27,18 @@ public class Json {
             return new JsonObject();
         }
     }
+
+    public static String toJson(Object obj) {
+        return new Gson().toJson(obj);
+    }
+
+    public static <T> T parseSafe(String json, Type t) {
+        try {
+            return new Gson().fromJson(json, t);
+        } catch (JsonSyntaxException e) {
+            SpiderDebug.log("json parse error: " + e.getMessage() + "\n" + " " + json);
+            return null;
+        }
+    }
+
 }
