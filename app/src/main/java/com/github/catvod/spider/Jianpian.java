541eac34d1bb28af4ece5d353a936e80e9abc5da
zwm001 <110072013+zwm001@users.noreply.github.com>
2025-07-02 15:58:01 +0800

    Update Jianpian.java

--- app/src/main/java/com/github/catvod/spider/Jianpian.java
+++ app/src/main/java/com/github/catvod/spider/Jianpian.java
@@ -8,13 +8,14 @@ import com.github.catvod.bean.Vod;
 import com.github.catvod.bean.jianpian.Data;
 import com.github.catvod.bean.jianpian.Detail;
 import com.github.catvod.bean.jianpian.Resp;
+import com.github.catvod.bean.jianpian.Search;
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.net.OkHttp;
-import com.github.catvod.utils.Json;
 
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,51 +24,55 @@ import java.util.Map;
  * Qile
  */
 public class Jianpian extends Spider {
-
-    private final String siteUrl = "http://api2.rinhome.com";
-    private String extend;
+    private static String siteUrl = "http://api.ubj83.com";
 
     private Map<String, String> getHeader() {
         Map<String, String> headers = new HashMap<>();
-        headers.put("User-Agent", "jianpian-android/360");
-        headers.put("JPAUTH", "y261ow7kF2dtzlxh1GS9EB8nbTxNmaK/QQIAjctlKiEv");
+        headers.put("user-agent", "Mozilla/5.0 (Linux; Android 11; Redmi K30 Pro Zoom Edition Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36;webank/h5face;webank/1.0;netType:NETWORK_WIFI;appVersion:416;packageName:com.jp3.xg3");
+        headers.put("accept", "application/json, text/plain, */*");
+        headers.put("x-requested-with", "com.jp3.xg3");
         return headers;
     }
 
     @Override
     public void init(Context context, String extend) throws Exception {
-        this.extend = extend;
+        if (!extend.isEmpty()) siteUrl = extend;
     }
 
     @Override
     public String homeContent(boolean filter) throws Exception {
         List<Class> classes = new ArrayList<>();
-        List<String> typeIds = Arrays.asList("0", "1", "2", "3", "4");
-        List<String> typeNames = Arrays.asList("全部", "电影", "电视剧", "动漫", "综艺");
-        for (int i = 0; i < typeIds.size(); i++) classes.add(new Class(typeIds.get(i), typeNames.get(i)));
-        return Result.string(classes, Json.parse(OkHttp.string(extend)));
+        List<String> typeIds = Arrays.asList("1", "2", "3", "4", "50", "99");
+        List<String> typeNames = Arrays.asList("电影", "电视剧", "动漫", "综艺", "纪录片", "Netflix");
+        for (int i = 0; i < typeIds.size(); i++)
+            classes.add(new Class(typeIds.get(i), typeNames.get(i)));
+
+        return Result.string(classes, Collections.emptyList());
     }
 
     @Override
     public String homeVideoContent() {
         List<Vod> list = new ArrayList<>();
-        String url = siteUrl + "/api/slide/list?code=unknown9039b6856c3a3306&pos_id=888&channel=wandoujia";
+        String url = siteUrl + "/api/slide/list?pos_id=88";
         Resp resp = Resp.objectFrom(OkHttp.string(url, getHeader()));
-        for (Data data : resp.getData()) list.add(data.vod());
+        for (Data data : resp.getData()) list.add(data.homeVod());
         return Result.string(list);
     }
 
     @Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
-        if (tid.endsWith("/{pg}")) return searchContent(tid.split("/")[0], pg);
         List<Vod> list = new ArrayList<>();
-        HashMap<String, String> ext = new HashMap<>();
-        if (extend != null && extend.size() > 0) ext.putAll(extend);
-        String cateId = ext.get("cateId") == null ? tid : ext.get("cateId");
-        String area = ext.get("area") == null ? "0" : ext.get("area");
-        String year = ext.get("year") == null ? "0" : ext.get("year");
-        String by = ext.get("by") == null ? "hot" : ext.get("by");
-        String url = siteUrl + String.format("/api/crumb/list?area=%s&category_id=%s&page=%s&type=0&limit=24&sort=%s&year=%s", area, cateId, pg, by, year);
+        String url;
+        if (tid.equals("50") || tid.equals("99")) {
+            url = siteUrl + String.format("/api/dyTag/list?category_id=%s", tid);
+            Resp resp = Resp.objectFrom(OkHttp.string(url, getHeader()));
+            for (Data data : resp.getData()) {
+                for (Data.DataList dataList : data.getDataList()) list.add(dataList.vod());
+            }
+            return Result.string(list);
+        } else {
+            url = siteUrl + String.format("/api/crumb/list?fcate_pid=%s&category_id=&area=&year=&type=&sort=&page=%s", tid, pg);
+        }
         Resp resp = Resp.objectFrom(OkHttp.string(url, getHeader()));
         for (Data data : resp.getData()) list.add(data.vod());
         return Result.string(list);
@@ -75,16 +80,16 @@ public class Jianpian extends Spider {
 
     @Override
     public String detailContent(List<String> ids) throws Exception {
-        String url = siteUrl + "/api/node/detail?channel=wandoujia&token=&id=" + ids.get(0);
+        String url = siteUrl + "/api/video/detailv2?id=" + ids.get(0);
         Data data = Detail.objectFrom(OkHttp.string(url, getHeader())).getData();
         Vod vod = data.vod();
-        vod.setVodPlayFrom("Jianpian");
+        vod.setVodPlayFrom(data.getVodFrom());
         vod.setVodYear(data.getYear());
         vod.setVodArea(data.getArea());
         vod.setTypeName(data.getTypes());
         vod.setVodActor(data.getActors());
-        vod.setVodPlayUrl(data.getPlayUrl());
-        vod.setVodDirector(data.getDirectors());
+        vod.setVodPlayUrl(data.getVodUrl());
+        vod.setVodDirector("Qile");
         vod.setVodContent(data.getDescription());
         return Result.string(vod);
     }
@@ -106,9 +111,9 @@ public class Jianpian extends Spider {
 
     public String searchContent(String key, String pg) throws Exception {
         List<Vod> list = new ArrayList<>();
-        String url = siteUrl + "/api/video/search?page=" + pg + "&key=" + URLEncoder.encode(key);
-        Resp resp = Resp.objectFrom(OkHttp.string(url, getHeader()));
-        for (Data data : resp.getData()) list.add(data.vod());
+        String url = siteUrl + String.format("/api/v2/search/videoV2?key=%s&category_id=88&page=%s&pageSize=20", URLEncoder.encode(key), pg);
+        Search search = Search.objectFrom(OkHttp.string(url, getHeader()));
+        for (Search data : search.getData()) list.add(data.vod());
         return Result.string(list);
     }
 }
